bd7fc4c9d96f8af397351ba239a186f283cbdff3
Lars Johnson <larsjohnson1851@gmail.com>
2026-05-05 14:32:44 -0600

    game_logic.py mostly completed

--- game_logic.py
+++ game_logic.py
@@ -1,4 +1,4 @@
-
+from copy import deepcopy
 
 
 # Lars Johnson
@@ -11,11 +11,53 @@ board_size = 9
 captured_black_stones = 0
 captured_white_stones = 0
 
+board = [[0] * board_size for i in range(board_size)]
+
+previous_board_state = deepcopy(board)
+
+turns_passed = 0
+
+game_over = False
+
 
 # This code will place a stone on the board, no rules applied at this moment...
 def place_stone(game_board, row, column, player):
-    game_board[row - 1][column - 1] = player
-    handle_captures(game_board, (row-1), (column-1))
+
+    global captured_black_stones, captured_white_stones, previous_board_state, game_over
+
+    if game_over == False:
+
+        ko_check_board = deepcopy(previous_board_state)
+        previous_board_state = deepcopy(game_board)
+
+        turns_passed = 0
+
+        if legal_move(game_board, (row - 1), (column - 1), player, ko_check_board) == 1:
+            game_board[row - 1][column - 1] = player
+            score = handle_captures(game_board, (row-1), (column-1))
+
+            #last_turn_board_state = deepcopy(previous_board_state)
+
+
+            if score != 0:
+                if player == 1:
+                    captured_white_stones = captured_white_stones + score
+                elif player == 2:
+                    captured_black_stones = captured_black_stones + score
+
+                print(f"Captured White Stones: {captured_white_stones}")
+                print(f"Captured Black Stones: {captured_black_stones}")
+
+
+        else:
+            print("This not a legal move!!!")
+
+    elif game_over == True:
+        print("The game is over!!!!!11")
+
+def place_stone_copy(game_board, row, column, player):
+    game_board[row][column] = player
+    handle_captures(game_board, (row), (column))
 
 
 # The goal of this function is to print the board state with either a ., a B, or a W.
@@ -52,9 +94,6 @@ def find_liberties(game_board, row, column):
     # now we need to do a loop for the beginning piece, if it is able to find more pieces throughout the process we need to somehow search those pieces as well
     while len(group) > 0:
 
-        #print(f"{game_board[group[0][0]][group[0][1]]}")
-        #print(f"{game_board[group[0][0]+1][group[0][1]]}")
-
         current = group.pop(0)
 
         r, c = current      # Gives the row of the stone
@@ -84,8 +123,6 @@ def find_liberties(game_board, row, column):
 def handle_captures(game_board, row, column):
     placed_stone_color = game_board[row][column]
 
-    global captured_white_stones, captured_black_stones
-
     offsets = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
     
@@ -106,27 +143,52 @@ def handle_captures(game_board, row, column):
                 # now we need to handle if the number of liberties are 0...
                 if len(liberties) == 0:
 
-                    if placed_stone_color == 1:
-                        captured_white_stones = captured_white_stones + len(group)
-                    elif placed_stone_color == 2:
-                        captured_black_stones = captured_black_stones + len(group)
 
                     for stones in group: # Capture the stones!!!
                         game_board[stones[0]][stones[1]] = 0
 
-                    
+                    return len(group)
 
-                    print(f"This is how many black stones captured :{captured_black_stones}")
-                    print(f"This is how many white stones captured :{captured_white_stones}")
+    
+    return 0
 
-                    # For this we need to get rid of the group with 0 liberties!!!
-                    #... But how?
 
-def legal_move(game_board, row, column):
-    # there are two things we need to do in this function...
-    # the first is to prevent the stone from being placed on an already placed stone
-    # The second is to prevent the stone from being immediately killed...
-    # The second one is the hard one...
+def legal_move(game_board, row, column, player, ko_check_board):
+    # First - prevent the stone from being placed on already placed stone.
+    if game_board[row][column] != 0:
+        return 0
+    
+    copied_game_board = deepcopy(game_board)
+    # Copy the game board.
+
+    place_stone_copy(copied_game_board, row, column, player)
+    # Place the piece on that copy.
+
+    copied_liberties, copied_group = find_liberties(copied_game_board, row, column)
+    # After the piece is placed, check the liberties on the piece.
+
+
+
+    if len(copied_liberties) == 0:
+        return 0
+    # if 0 liberties, then its an illegal move
+
+    if ko_check_board == copied_game_board:
+        return 0
+
+    return 1
+    # Return 1 for a legal move!
+
+
+
+def pass_turn(player):
+    global turns_passed, game_over
+
+    if turns_passed == 0:
+        turns_passed = turns_passed + 1
+
+    elif turns_passed == 1:
+        game_over = True
 
     
 
@@ -136,9 +198,10 @@ def legal_move(game_board, row, column):
 
 
 
-# First: creating the basic board state.
 
-board = [[0] * board_size for i in range(board_size)]
+
+
+# First: creating the basic board state.
 
 place_stone(board, 2, 7, 1)
 
@@ -152,9 +215,24 @@ place_stone(board, 4, 4, 1)
 place_stone(board, 4, 6, 1)
 place_stone(board, 5, 5, 1)
 
-print_board(board, board_size)
+
+#print_board(board, board_size)
+
 
 place_stone(board, 3, 5, 1)
 
-print_board(board, board_size)
+#place_stone(board, 3, 5, 1)
 
+#print_board(board, board_size)
+
+place_stone(board, 4, 5, 2)
+
+#print_board(board, board_size)
+
+place_stone(board, 3, 5, 1)
+
+pass_turn(1)
+
+pass_turn(2)
+
+place_stone(board, 1, 1, 1)
